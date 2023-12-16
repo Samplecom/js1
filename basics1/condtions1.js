6b77ee1ed4f2e8ccb0d699d371e050dc4408dc3d
Rathlavath Suman <119931096+Samplecom@users.noreply.github.com>
2023-12-16 17:43:58 +0530

    Add files via upload

---
+++ basics1/condtions1.js
@@ -0,0 +1,237 @@
+Q1. What are conditional statements? Explain conditional statements with syntax and examples.
+
+
+
+ the conditional statements are :
+  
+ 1. if statement 
+   syntax:
+
+    if(){
+--------
+---------
+              condition   
+}
+
+   
+    const x = 10
+
+    if(x < 5){
+
+ console.log(" yes it is less than ");
+}
+
+ 
+2. if else
+   
+ syntax:
+
+    if(){
+--------
+---------
+              condition   
+}else{
+
+---- - - - ---
+-
+-
+-
+-
+}
+
+
+example:
+
+ const x = 10
+
+    if(x < 5){
+
+ console.log(" yes it is less than ");
+}
+else{
+
+ console.log(" not less than ");
+}
+
+
+3. if else if 
+
+sytax:
+
+  if()
+{
+
+
+condtion
+}else if(){
+
+statment
+}else{
+
+statement
+
+}
+
+
+
+example:
+
+let x = 30
+
+ if(x < 20){
+
+ console.log("not")
+}
+else if(x >10){
+
+console.log("yes grater than")
+}else{
+
+console.log( "less than it")
+}
+
+
+******************************************************
+ 2. 
+
+let studentMarks = 90;
+let grade;
+
+if (studentMarks >= 90) {
+    grade = 'A';
+} else if (studentMarks >= 80) {
+    grade = 'B';
+} else if (studentMarks >= 70) {
+    grade = 'C';
+} else if (studentMarks >= 60) {
+    grade = 'D';
+} else {
+    grade = 'F';
+}
+
+console.log(`Student marks: ${studentMarks}`);
+console.log(`Student grade: ${grade}`);
+
+
+****************************************************************
+
+3. loops 
+
+ their are different types of loops they are:
+  
+ 1. for loop
+ 2. while loop
+ 3. do while loop
+
+ 1. for loop
+
+ syntax:
+ 
+ for(condition){
+
+}
+ 
+ example:
+
+ 
+for(let i = 10; i >= 10; i++){
+console.log(i)
+}
+ 
+ while loop
+
+  syntax:
+ 
+ while(condition){
+
+  statement
+
+}
+
+example:
+  
+ let x = 0
+
+ while(x < 5)
+
+  console.log(x)
+  
+   i++
+}
+
+ do while
+ syntax:
+ do{
+
+ }
+while(conditional);
+
+example:
+
+  let x = 0;
+do{
+
+ console.log(x)
+
+  x++
+}
+while(x < 20);
+ ***************************************************************************************
+4.
+
+
+
+ let i = 10;
+let x = 20;
+
+for(let y = i; y <= x; y++)
+console.log(y);
+
+ *******************************************************************
+5. 
+ 
+ 
+accending order:
+
+   let i = 1
+while(i <= 20){
+    console.log(i)
+    i++
+}
+ 
+decending order:
+  
+ let i = 20
+
+ while(i >= 1){
+
+console.log(i)
+
+ i--
+}s
+ 
+
+
+
+
+  
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
